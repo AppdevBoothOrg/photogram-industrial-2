3ca6485c9866a3978c40555a0fa857fb42fa5758
ibrahimmc <imoufarr@chicagobooth.edu>
2021-04-22 21:20:53 +0000

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
 
   get ":username/liked" => "photos#liked", as: :liked_photos
   get ":username/feed" => "photos#feed", as: :feed_photos
-  get ":username/followers" => "photos#liked", as: :followers_list
-  get ":username/following" => "photos#liked", as: :following_list
+  
+  get ":username/followers" => "photos#followers", as: :followers
+  get ":username/following" => "photos#following", as: :following
   
   get ":username" => "users#show", as: :user
 end
